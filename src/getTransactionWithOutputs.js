49f097b4004c39dcbfb90ce2873bf0a2be73afb5
Ty Everett <ty@tyweb.us>
2020-11-23 14:37:44 -0800

    Update gtwo

--- src/getTransactionWithOutputs.js
+++ src/getTransactionWithOutputs.js
@@ -1,23 +1,27 @@
 const createSignedRequest = require('./utils/createSignedRequest')
 const bsv = require('bsv')
 const rpuzzle = require('rpuzzle')
+const { CONFIG } = require('./defaults')
 
 module.exports = async ({
   xprivKey,
-  config,
+  config = CONFIG,
   outputs,
   rPuzzleInputSigningWIF,
   feePerKb = 500
-}) => {
+} = {}) => {
   const hdKey = bsv.HDPrivateKey.fromString(xprivKey)
   const tx = new bsv.Transaction()
 
   outputs.forEach(out => {
-    tx.to(bsv.Script.fromString(out.script), out.satoshis)
+    tx.addOutput(new bsv.Transaction.Output({
+      script: new bsv.Script(out.script),
+      satoshis: out.satoshis
+    }))
   })
 
   const requiredOutputs = outputs.map(o => ({
-    satoshis: o.satoshis,
+    amount: o.satoshis,
     scriptLength: o.script.length / 2
   }))
 
@@ -38,18 +42,21 @@ module.exports = async ({
 
   // Add the change outputs
   changeOutputs.forEach(output => {
-    tx.to(bsv.Script.fromString(output.script), output.amount)
+    tx.addOutput(new bsv.Transaction.Output({
+      script: bsv.Script.fromString(output.script),
+      satoshis: output.satoshis
+    }))
   })
 
   // Add inputs
   for (let i = 0; i < selectedInputs.length; i++) {
     const input = selectedInputs[i]
-    tx.from({
+    tx.from(bsv.Transaction.UnspentOutput({
       txid: input.txid,
       outputIndex: input.vout,
       scriptPubKey: input.lockingScript,
       satoshis: input.amount
-    })
+    }))
   }
 
   //  Sign inputs based on the input type
@@ -63,19 +70,18 @@ module.exports = async ({
       const signingKey = typeof rPuzzleInputSigningWIF === 'undefined'
         ? bsv.PrivateKey.fromRandom()
         : bsv.PrivateKey.fromWIF(rPuzzleInputSigningWIF)
-      const puz = rpuzzle(rpuzzle.KValue.fromHex(kHex))
-      puz.setPrivateKey(signingKey)
+      const puz = rpuzzle(rpuzzle.KValue.fromHex(kHex), signingKey)
       puz.sign(tx)
     } else {
       throw new Error(
-      `The server is specifying ${input.type}, which is an unknown input unlocking script type to be used`
+        `The server is specifying ${input.type}, which is an unknown input unlocking script type to be used`
       )
     }
   }
 
   // Return serialized hex and reference
   return {
-    hex: tx.serialize(),
+    hex: tx.uncheckedSerialize(),
     reference
   }
 }
